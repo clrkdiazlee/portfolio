0b27e28d21f3d02e9aa5996308ec5c1c4231f0d5
clrkdiazlee <clrkdiazlee@gmail.com>
2024-04-30 01:47:53 +0800

    added to repo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className=''>
+      <div className='w-full border-b-[1px] border-black h-20 flex'>
+        <div className='w-20 border-r-[1px] border-black h-20'>
+
+        </div>
+        <div>
+          asd
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
